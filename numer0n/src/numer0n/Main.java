740bc64dcac1d37238cda71f111f869d3762f203
Ryoga Murata <wassyoi.ryo0724@icloud.com>
2019-07-09 22:46:46 +0900

    コメント追加

--- numer0n/src/numer0n/Main.java
+++ numer0n/src/numer0n/Main.java
@@ -12,15 +12,27 @@ public class Main {
         Player player = new Player();
         Comp comp = new Comp();
 
+        /**
+         * BITE数を表す
+         */
         int numberOfBite = 0;
+
+        /**
+         * EAT数を表す
+         */
         int numberOfEat = 0;
 
         System.out.println("数字を決めてね");
 
+        //プレーヤーの数字を決める
         player.setUp();
+        //相手の数字を決める
         comp.setUp();
 
-        //順番関係なしに確かめる
+        //順番関係なしに確かめる（とりあえず作った）
+        /**
+         * callする3桁の数字を表す
+         */
         ArrayList<Integer> callNumbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
@@ -28,11 +40,13 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             System.out.println(i + 1 + "けた目の数字を入力してね");
             int inputNumber = scanner.nextInt();
+
             if (inputNumber < 0 || 9 < inputNumber) {
                 System.out.println("0~9の間で入力しろ");
                 i--;
                 continue;
             }
+
             callNumbers.add(inputNumber);
             for (int j = 0; j < i; j++) {
                 if (callNumbers.get(i).equals(callNumbers.get(j))) {
@@ -57,12 +71,12 @@ public class Main {
 
         //確認用プレーヤーの数字表示
         for (int i = player.getNumbers().size() - 1; 0 <= i; i--) {
-            System.out.print(player.getNumbers().get(i));
+            System.out.print("自分の数字" + player.getNumbers().get(i));
         }
         System.out.println();
         //確認用相手の数字表示
         for (int i = comp.getNumbers().size() - 1; 0 <= i; i--) {
-            System.out.print(comp.getNumbers().get(i));
+            System.out.print("相手の数字" + comp.getNumbers().get(i));
         }
         System.out.println();
 
